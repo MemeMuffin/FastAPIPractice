d4ad47ac759bc000b0ab08d984e9133147d603f8
MemeMuffin <105114920+MemeMuffin@users.noreply.github.com>
2025-04-11 11:43:17 +0500

    Update db.py
    Removed personal info

--- config/db.py
+++ config/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb+srv://mutayyabh02:hafeez12@cluster0.s9ye04b.mongodb.net/notes"
+MONGO_URI = #Enter your DB connection here
 
 conn = MongoClient(MONGO_URI)
